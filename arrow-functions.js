cc243b5fcf4b3c153a9ee1e49fd3a9a3da6489c6
Sndrahel <sandra.helander@hotmail.com>
2021-07-28 11:41:49 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,7 +5,21 @@
 
 // Vanilla JavaScript Function
 
+//function addTwoNumbers(a, b) {
+    //Code block
+//    return a + b;
+//}
+//let sum = addTwoNumbers(3, 5);
+//console.log(sum);
+
 // Arrow Function With Parameters
+
+function addTwoNumbers(a, b) {
+    //Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
