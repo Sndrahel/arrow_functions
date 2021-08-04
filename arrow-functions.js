e6c56923fbda6adcd159dd52af6405a1cec28bf6
Sndrahel <sandra.helander@hotmail.com>
2021-08-04 15:14:41 +0000

    Add map-method.js

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,7 +4,7 @@
  */
 
 // Vanilla JavaScript Function
-
+/*
 function addTwoNumbers(a, b) {
     //Code block
     return a + b;
@@ -45,3 +45,121 @@ const returnMultipleLines = () => (
     </p>`
 )
 console.log(returnMultipleLines());
+
+/**
+ * To run this file in Gitpod, use the 
+ * command node the-spread-operator.js in the terminal
+ */
+/*
+// No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4);
+console.log("Second array:", arr2);
+console.log("First array:", arr1);
+
+// Copying an array
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
+
+arr4.push(7);
+
+console.log("Forth array:", arr4);
+console.log("Third array:", arr3);
+
+// Copying an object
+let obj1 = {a: 1, b: 2, c: 3};
+let obj2 = {...obj1, d: 4};
+let obj3 = {...obj1, b: 5};
+
+console.log("First object:", obj1);
+console.log("Second object:", obj2);
+console.log("Third object:", obj3);
+
+// Copying only part of an array/object
+let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
+
+console.log(arr5);
+*/
+/**
+ * To run this file in Gitpod, use the 
+ * command node rest-parameter-syntax.js in the terminal
+ */
+
+// Regular function call 
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log("Sum:", sum);
+
+// Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log(sum2);
+
+// Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log(sum3);
+
+/**
+ * To run this file in Gitpod, use the 
+ * command node destructuring.js in the terminal
+ */
+
+// Destructuring arrays
+let ages = [30, 26, 27];
+//let john = ages[0];
+//let john = ages[1];
+//let john = ages[2];
+let [john, mary, joey] = ages;
+console.log(john, mary, joey)
+
+// Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+}
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia)
+
+// Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary)
+
+let [,, maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary)
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "german",
+    fourthLanguage: "japanese",
+};
+let {firstLanguage, thirdLanguage} = languages2;
+console.log(firstLanguage, thirdLanguage)
+
+// Using rest parameter syntax
+let fruits = ["apple",  "orange", "banana", "peach", "cherry"];
+let [favorite, secondFavorite, ...others] = fruits;
+console.log(favorite)
+console.log(secondFavorite)
+console.log(others)
+
+let favoriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak",
+};
+let {brian, anna, ...rest} = favoriteFoods;
+console.log(brian)
+console.log(anna)
+console.log(rest) 
